de489a2f6ddd1de2d7f24f5bc39b3ab8b1af0fea
Eric G <peaceandhealth@protonmail.com>
2021-09-19 19:05:43 -0400

    Added divs into DOM

--- files/logic.js
+++ files/logic.js
@@ -1,64 +1,15 @@
 const container = document.querySelector(".container");
 
-//creates a div and appends it to the container div
-function createSpan1 () {
-    const span = document.querySelector('span')
-    span.classList.add('grid-row1');
-    container.append(span);
+function createGrid2 (rows, cols) {
+    for (let i = 0; i <= (rows * cols); i++) {
+        const div = document.createElement('div');
+        div.classList.add('block');
+        div.textContent = i;
+        container.append(div);
+    }
 }
 
-function createSpan2 () {
-    const span = document.querySelector('span')
-    span.classList.add('grid-row2');
-    container.append(span);
-}
+createGrid2 (16,16);
 
-function createSpan3 () {
-    const span = document.querySelector('span')
-    span.classList.add('grid-row3');
-    container.append(span);
-}
 
-function createDiv1 () {
-    const span1 = document.querySelector('.grid-row1')
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    span1.append(div);
-}
-
-function createDiv2 () {
-    const span2 = document.querySelector('.grid-row2')
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    span2.append(div);
-}
-
-function createDiv3 () {
-    const span3 = document.querySelector('.grid-row3')
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    span3.append(div);
-}
-
-function createGrid () {
-    createSpan1();
-    createDiv1();
-    createDiv1();
-    createDiv1();
-    createDiv1();
-    
-    createSpan2();
-    createDiv2();
-    createDiv2();
-    createDiv2();
-    createDiv2();
-
-    createSpan3();
-    createDiv3();
-    createDiv3();
-    createDiv3();
-    createDiv3();
-
-}
-
-createGrid ()
+// create divs -> organize on the page ->
