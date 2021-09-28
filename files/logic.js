0d3b9f3bbe821593cf1e308a9c6390b7dfc0b72e
Eric G <peaceandhealth@protonmail.com>
2021-09-28 13:19:13 -0400

    added sketch feature

--- files/logic.js
+++ files/logic.js
@@ -9,7 +9,30 @@ function createGrid (rows, cols) {
     }
 }
 
+function changeColor (event) {
+    event.target.style.backgroundColor = "#4b14e0";
+    //event.target.style.opacity += 0.1;
+}
+
 createGrid (16,16);
 
+const blocks = document.querySelectorAll(".block");
+blocks.forEach(block => block.addEventListener("mouseover", changeColor));
+console.log (blocks);
+
+// while (run == true) {
+//     let i = 0;
+//     let selection = block[i];
+//     document.addEventListener('DOMContentLoaded', function () {
+//         selection.addEventListener("mouseover", function(e) {
+//             block.id = 'colored';
+//             i++
+
+//         if (i == 256) {
+//             i = 0;
+//         }
+//         })
+//     });
+// }
 
 // create divs -> organize on the page ->
